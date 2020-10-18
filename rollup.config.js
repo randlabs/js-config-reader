10d1cb0de18bbdadbc95e550fbb2cd82bd54ffad
Mauro Leggieri <mxmauro@users.noreply.github.com>
2020-10-17 21:47:56 -0300

    Bundler + optional options (#6)
    * Added draft-2019
    
    * Fixed location of library in tests
    
    * Set version to v1.0.1
    
    * Options are optional
    
    * Added bundler

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable global-require */
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import ts from "@wessberg/rollup-plugin-ts";
+import json from "@rollup/plugin-json";
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+export default [
+	{
+		input: './src/index.ts',
+		output: {
+			name: "UInt64",
+			file: pkg.main,
+			format: 'cjs',
+			sourcemap: true,
+			exports: "auto"
+		},
+		external: [ "fs", "path", "util", "ajv", "ajv-formats-draft2019", "json5" ],
+		plugins: [
+			json(),
+			ts({
+				tsconfig: "./tsconfig.json"
+			}),
+			commonjs(),
+			resolve(),
+			terser({
+				ecma: 2015
+			})
+		]
+	}
+];
